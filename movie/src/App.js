567a0696e6f759121b6143d477e356c106ffb810
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2022-08-08 03:02:20 +0900

    ver2

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,23 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
-asdasd;
